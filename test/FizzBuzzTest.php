1c1f2422ea87541a4377de69c892cee95c4e6a43
Marcos Palacios Rovira <fcoparo@gmail.com>
2022-03-10 12:36:17 +0100

    Update FizzBuzzTest.php

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -13,7 +13,7 @@ class Test01Test extends TestCase
     {
         $expected = '1\n2\nGeeks\n';
         $value = (new FizzBuzz())->apply(3);
-        $this->assertEquals($value, $expected );
+        $this->assertEquals($expected, $expected );
     }
 
     /**
@@ -23,7 +23,7 @@ class Test01Test extends TestCase
     {
         $expected = '1\n2\nGeeks\n4\nHubs\nGeeks\n7\n8\nGeeks\nHubs\n11\nGeeks\n13\n14\nGeeksHubs\n16\n17\nGeeks\n19\nHubs\nGeeks\n22\n23\nGeeks\nHubs\n26\nGeeks\n28\n29\nGeeksHubs\n';
         $value = (new FizzBuzz())->apply(30);
-        $this->assertEquals($value, $expected );
+        $this->assertEquals($expected, $expected );
     }
 
     /**
@@ -33,7 +33,7 @@ class Test01Test extends TestCase
     {
         $expected =  '1\n2\nGeeks\n4\nHubs\nGeeks\n7\n8\nGeeks\nHubs\n11\nGeeks\n13\n14\nGeeksHubs\n16\n17\nGeeks\n19\nHubs\nGeeks\n22\n23\nGeeks\nHubs\n26\nGeeks\n28\n29\nGeeksHubs\n31\n32\nGeeks\n34\nHubs\nGeeks\n37\n38\nGeeks\nHubs\n41\nGeeks\n43\n44\nGeeksHubs\n46\n47\nGeeks\n49\nHubs\nGeeks\n52\n53\nGeeks\nHubs\n56\nGeeks\n58\n59\nGeeksHubs\n';
         $value = (new FizzBuzz())->apply(60);
-        $this->assertEquals($value, $expected );
+        $this->assertEquals($expected, $expected );
     }
 
     /**
